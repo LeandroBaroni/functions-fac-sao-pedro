a569e1074459d494fd871db0cf79f769babfa465
LeandroBaroni <leandrobaroni2103@hotmail.com>
2025-06-24 22:39:05 -0300

    Primeiro commit

---
+++ src/main/java/com/leandrobaroni2103/functions_fac_sao_pedro/FunctionsFacSaoPedroApplication.java
@@ -0,0 +1,13 @@
+package com.leandrobaroni2103.functions_fac_sao_pedro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionsFacSaoPedroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionsFacSaoPedroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leandrobaroni2103/functions_fac_sao_pedro/FunctionsFacSaoPedroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandrobaroni2103.functions_fac_sao_pedro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionsFacSaoPedroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
